f5f73d0e8ebe1e9ba56f9e7a9ffe59ac5471f875
anveraantonio <103013837+anveraantonio@users.noreply.github.com>
2022-05-05 21:42:46 -0400

    li
    li

--- application.php
+++ application.php
@@ -1,18 +1,42 @@
 
 <?php 
- $nombre = $_POST['nombre'];
- $email = $_POST['email'] ;
- $fecha = $_POST['fecha'];
- $fono = $_POST['fono'];
- $edad = $_POST['edad'];
- //echo "I love $nombre, $email, $fecha, $fono!";
-$misolicitud = new Solicitud($_POST['nombre'], $_POST['email'], $_POST['fecha'], $_POST['fono'], $_POST['edad']);
+Class Solicitud{
+    public $nombre;
+    public $email;
+    public $fecha;
+    public $fono;
+    public $edad;
+    public $current_year;
 
-$misolicitud2 = new Solicitud($nombre, $email, $fecha, $fono, $edad);
-echo "Se realiza GET desde la clase creada como 'misolicitud \n";
-echo $misolicitud->get_nombre(); 
+    function __construct($nombre, $email, $fecha, $fono, $edad){
+        $this->nombre = $nombre;
+        $this->email = $email;
+        $this->fecha = $fecha;
+        $this->fono = $fono;
+        $this->edad = $edad;
+    }
+    function set_nombre($nombre){
+        $this->nombre = $nombre;
+        
+    }
+
+
+    function get_nombre(){
+        return $this->nombre;
+    }
+    function get_email(){
+        return $this->email;
+    }
+    function get_fecha(){
+        return $this->fecha;
+    }
+    function calc_edad(){
+        $this->current_year = date('Y');
+        return $this->current_year-$this->edad;
+    }
+}
+$misolicitud = new Solicitud($_POST['nombre'], $_POST['gmail'], $_POST['fecha'], $_POST['fono'], $_POST['edad']);
 echo $misolicitud->get_email();
-echo $misolicitud->get_fecha();
-echo $misolicitud->get_fono();
-echo $misolicitud->get_edad();
+echo '<br>';
+echo $misolicitud->calc_edad();
 ?>
